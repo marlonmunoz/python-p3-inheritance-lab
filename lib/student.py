24f066d9dc2ee401c1f807996ed50f2e7f8a5a73
Marlon Munoz <marlon@marlonshade.com>
2024-08-28 16:17:05 -0400

    lab part one

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = knowledge if knowledge is not None else []
     
-    def learn(self):
+    def learn(self, new_info):
+        self.knowledge.append(new_info)
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,11 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = knowledge if knowledge is not None else ["default knowledge"]
+
     def teach(self):
+        return random.choice(self.knowledge)
         pass
